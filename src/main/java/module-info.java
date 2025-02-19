29d777b70aa65e6ff2ad70bac2d3bdf4bf2f1418
David Mateo Murcia <mmurcia@ol4b.com>
2025-02-19 11:26:37 -0500

    PruebaGitDevel2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module com.ejemplo {
     requires javafx.fxml;
     requires java.sql;
 
-    opens com.ejemplo to javafx.fxml;  // Asegura que las clases de FXML se puedan abrir para JavaFX
+    opens com.ejemplo to javafx.fxml;  
     exports com.ejemplo;  // Exporta el paquete raíz
     exports com.ejemplo.view to javafx.graphics;  // Exporta el paquete view a javafx.graphics
 }
